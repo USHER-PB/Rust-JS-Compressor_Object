75b1369af27b9edd0fd43771f44a0884450f2e32
USHER-PB <ushertchankoumi9@gmail.com>
2025-04-22 12:51:36 +0100

    Feat:modify src file

--- rustcompressor_lz77/src/main.rs
+++ rustcompressor_lz77/src/main.rs
@@ -1,31 +1,60 @@
-use std::{env, fs};
+use std::{env, fs, io::{self, Read, Write}};
 use rustcompressor_lz77::{compress_lz77, decompress_lz77};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let choice = &args[1];
-    let input_path = &args[2];
-    let output_path = &args[3];
 
-    let contents = match fs::read_to_string(input_path) {
-        Ok(c) => c,
-        Err(e) => {
-            eprintln!("Error reading file: {}", e);
-            return;
+    // Determine operation mode
+    let (choice, input_data): (String, Vec<u8>) = if args.len() >= 2 {
+        let choice = args[1].clone();
+        let input_data = if args.len() >= 3 {
+            fs::read(&args[2]).expect("Failed to read input file")
+        } else {
+            let mut buffer = Vec::new();
+            match io::stdin().read_to_end(&mut buffer){
+                Ok(buffer) => buffer,
+                Err(e) => {
+                    eprintln!("no file present:{}", e);
+                    return
+                }
+            };
+            buffer
+           
+        };
+        (choice, input_data)
+    } else {
+        eprintln!("Usage: <compress|decompress> [input_file] [output_file]");
+        std::process::exit(1);
+    };
+
+    // Run compression or decompression
+    let result = if choice == "compress" {
+        compress_lz77(&String::from_utf8_lossy(&input_data))
+    } else if choice == "decompress" {
+        decompress_lz77(&input_data).into_bytes()
+    } else {
+        eprintln!("Unknown command: {}", choice);
+        std::process::exit(1);
+    };
+
+    // Write to output file or stdout
+    if args.len() >= 4 {
+        match fs::write(&args[3], &result){
+            Ok(_) => {
+                println!(" [process completed result in  :{:?}", &args[3]);
+            }
+            Err(e) =>  {
+                eprintln!("NO Output file found:{}", e);
+            }
+        };
+    } else {
+        match io::stdout().write_all(&result){
+        Ok(_) => {
+            println!("file sucefulllt compressed to :{:?}", result);
+        }
+        Err(e) =>  {
+            eprintln!("NO Output file found:{}", e);
         }
     };
-
-    let result = if choice == "compress" {
-        compress_lz77(&contents) // Use compress_lz77
-    } else if choice == "decompress" {
-        decompress_lz77(contents.as_bytes()).into_bytes() // Convert String to Vec<u8>
-    } else {
-        eprintln!("Unknown choice: {}", choice);
-        return;
-    };
-
-    match fs::write(output_path, &result) {
-        Ok(_) => println!("{} completed.", choice),
-        Err(e) => eprintln!("Failed to write to output: {}", e),
     }
 }
